e6acd6196014f3d60f41ad13e804eac762ce3d87
gohg0901 <gohg0901@naver.com>
2023-03-14 18:09:38 +0900

    main 페이지 작성

--- Dufs-FrontEnd/src/App.js
+++ Dufs-FrontEnd/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./pages/Header"
 import Footer from "./pages/Footer"
+import Main from "./pages/Main"
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <div className='contentWrapper'>
-        <h2>body</h2>
+        <Main />
       </div>
       <Footer />
     </div>
---
+++ Dufs-FrontEnd/src/pages/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Main = (props) => {
+	return (
+		<>
+			<h3>Main Page</h3>
+		</>
+	);
+};
+
+export default Main;
