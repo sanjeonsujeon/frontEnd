32c6e04b4b174ddf158db5a7726da431a005e3a6
Hamtoto <gohg0901@naver.com>
2023-06-10 02:55:47 +0900

    관리용 페이지 제거, 메인페이지 직링크 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,25 @@ import styled from "styled-components";
 //import component
 import Header from './Component/Header';
 import Footer from './Component/Footer';
+
 //import pages
 import Main from './pages/Main';
 import Login from './pages/Login';
 import SignUp from './pages/SginUp';
 import NotFound from './pages/NotFound';
-import DevPage from "./pages/TestPage";
 import Board from "./pages/Board";
 import CreateBoard from "./pages/CreateBoard";
 import BoardDetail from "./pages/Detail";
 import UpdatePost from "./pages/UpdatePost";
+
 //import config
 import config from "./config";
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: auto;
-  min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    height: auto;
+    min-height: 100vh;
 `
 
 const Content = styled.div`
@@ -40,10 +41,10 @@ const Content = styled.div`
 `
 //  width: 75%;
 const AppRoutes = styled.div`
-  width: 100%;
-  display: flex;
-  height: auto;
-  justify-content: center;
+    width: 100%;
+    display: flex;
+    height: auto;
+    justify-content: center;
 `;
 
 function App() {
@@ -55,7 +56,6 @@ function App() {
         <AppRoutes>
           <Routes>     
             <Route path="/" element = {<Main/>} />
-            <Route path="/devpage" element = {<DevPage/>} />
             <Route path="/login" element = {<Login/>} />
             <Route path="/sginup" element = {<SignUp/>} />
             <Route path="/board" element = {<Board/>} />
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import styled from "styled-components";
+import { NavLink } from 'react-router-dom';
+import SidebarItem from '../Component/NavItem';
 
 const BodyContent = styled.div`
-	display: flex;
-	width: 100%;
-	flex-direction: column;
+display: flex;
+flex-direction: column;
+`
+const Menu = styled.div`
+margin: auto;
+display: flex;
+flex-direction: row;
 `
 
 const Main = (props) => {
+	const menus = [
+        { name: "게시판", path: "/board"},
+        { name: "로그인", path: "/login"},
+        { name: "회원가입", path: "/sginup"},
+        { name: "404 페이지", path: "/*"}
+      ];
 
 	return (
 		<>
 			<BodyContent>
-				메인
+				<Menu>
+				{menus.map((menu, index) => {   
+					return (
+						<NavLink
+							exact
+							style={{color: "Black", textDecoration: "none", margin: "10px"}}
+							to={menu.path} 
+							key={index}
+							activeStyle={{color: "black"}}
+						>
+							<SidebarItem menu={menu} />
+						</NavLink>
+					);
+				})}
+				</Menu>
 			</BodyContent>
 		</>
 	);
