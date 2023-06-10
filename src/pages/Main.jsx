d75a54909babaa2dba93ede6b9298a0dd021aa48
Hamtoto <gohg0901@naver.com>
2023-06-11 02:48:27 +0900

    메인 디자인 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,46 +2,59 @@ import React from 'react';
 import styled from "styled-components";
 import { NavLink } from 'react-router-dom';
 import SidebarItem from '../Component/NavItem';
+import sampleimg from "../images/samplebanner.png";
 
 const BodyContent = styled.div`
-display: flex;
-flex-direction: column;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 `
+
 const Menu = styled.div`
-margin: auto;
-display: flex;
-flex-direction: row;
+  margin: auto;
+  display: flex;
+  flex-direction: row;
+`
+
+const Banner = styled.div`
+  width: 100%;
+  height: 500px;
+  background-size: cover;
+  background-position: center;
+  margin: 20px auto;
+  background-image: url(${sampleimg});
 `
 
 const Main = (props) => {
-	const menus = [
-        { name: "게시판", path: "/board"},
-        { name: "로그인", path: "/login"},
-        { name: "회원가입", path: "/signup"},
-        { name: "404 페이지", path: "/*"}
-      ];
+  const menus = [
+    { name: "게시판", path: "/board" },
+    { name: "로그인", path: "/login" },
+    { name: "회원가입", path: "/signup" },
+    { name: "404 페이지", path: "/*" }
+  ];
 
-	return (
-		<>
-			<BodyContent>
-				<Menu>
-				{menus.map((menu, index) => {   
-					return (
-						<NavLink
-							exact
-							style={{color: "Black", textDecoration: "none", margin: "10px"}}
-							to={menu.path} 
-							key={index}
-							activeStyle={{color: "black"}}
-						>
-							<SidebarItem menu={menu} />
-						</NavLink>
-					);
-				})}
-				</Menu>
-			</BodyContent>
-		</>
-	);
+  return (
+    <>
+      <BodyContent>
+        <Banner />
+        <Menu>
+          {menus.map((menu, index) => {
+            return (
+              <NavLink
+                exact
+                style={{ color: "Black", textDecoration: "none", margin: "10px" }}
+                to={menu.path}
+                key={index}
+                activeStyle={{ color: "black" }}
+              >
+                <SidebarItem menu={menu} />
+              </NavLink>
+            );
+          })}
+        </Menu>
+      </BodyContent>
+    </>
+  );
 };
 
 export default Main;
