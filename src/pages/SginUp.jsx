9ff02b2f22b355d0a47ef888adee623192aa6005
Hamtoto <gohg0901@naver.com>
2023-06-12 05:46:33 +0900

    공백제거, 비밀번호 일치 기능 추가,

--- src/pages/SginUp.jsx
+++ src/pages/SginUp.jsx
@@ -9,7 +9,7 @@ const SignUpContent = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-`;
+`
 
 const SignUpForm = styled.div`
   width: 300px;
@@ -17,11 +17,11 @@ const SignUpForm = styled.div`
   border: 1px solid #ccc;
   border-radius: 5px;
   background-color: #f0f0f0;
-`;
+`
 
 const Title = styled.h2`
   margin-bottom: 20px;
-`;
+`
 
 const Input = styled.input`
   width: 280px;
@@ -29,7 +29,7 @@ const Input = styled.input`
   margin-bottom: 10px;
   border-radius: 5px;
   border: 1px solid #ccc;
-`;
+`
 
 const Label = styled.label`
   display: block;
@@ -37,26 +37,25 @@ const Label = styled.label`
   text-align: left;
   color: #333;
   font-weight: bold;
-`;
+`
 
 const SignUp = () => {
+	const navigate = useNavigate();
 	const [userid, setUserIdInput] = useState("");
 	const [pw, setPasswordInput] = useState("");
 	const [pwcheck, setPasswordCheck] = useState("");
 	const [name, setnameInput] = useState("");
 	const [stnumber, setstudent_numberInput] = useState("");
 
-	const navigate = useNavigate();
-
 	const idCheck = () => {
-		axios.post("http://localhost:8080/api/IDcheck", null, {
+		axios.post("api/IDcheck", null, {
 			params: {
 				userid: userid
 			}
-			})
+		})
 			.then((response) => {
 				if ((response.status === 200)) {
-					return alert("쌉가능");
+					return alert("사용가능한 ID 입니다.");
 				}
 			})
 			.catch((error) => {
@@ -65,38 +64,47 @@ const SignUp = () => {
 	};
 
 	const requestData = { userid, pw, name, stnumber };
+
+	const handlePasswordChange = (e) => {
+		setPasswordInput(e.target.value);
+	};
+
+	const handlePasswordCheckChange = (e) => {
+		setPasswordCheck(e.target.value);
+	};
+
 	const registeraxios = () => {
-		const koreanRegex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; // 한글 체크 정규식
-		const specialCharRegex = /[~`!@#$%^&*()+=\-[\]\\';,./{}|":<>?_]/; // 특수문자 체크 정규식
+		const koreanRegex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+		const specialCharRegex = /[~`!@#$%^&*()+=\-[\]\\';,./{}|":<>?_]/;
 		if (userid.trim() === "" || pw.trim() === "") {
-			// 아이디가 공란인 경우
 			return alert("아이디 또는 비밀번호를 입력해주세요!");
 		}
 		if (name.trim() === "") {
-			// 아이디가 공란인 경우
 			return alert("이름을 입력해주세요!");
 		}
 		if (stnumber.trim() === "") {
-			// 아이디가 공란인 경우
 			return alert("학번을 입력해주세요!");
 		}
 		if (
 			koreanRegex.test(userid) || specialCharRegex.test(userid) ||
 			koreanRegex.test(pw) || specialCharRegex.test(pw)
 		) {
-			// 아이디나 비밀번호에 한글이나 특수문자가 포함된 경우
 			return alert("아이디 또는 비밀번호에는 한글과 특수문자를 사용할 수 없습니다!");
+		}
+		if (pw !== pwcheck) {
+			return alert("비밀번호가 일치하지 않습니다!");
 		}
 
 		axios
 			.post(`/api/signup-pp`, requestData)
 			.then((response) => {
 				if ((response.status = 200)) {
-					return alert("회원가입 완료"), navigate("/Login");
+					alert("회원가입 완료");
+					return navigate("/Login");
 				}
 			})
 			.catch((error) => {
-				return alert("회원가입에 실패했습니다!");
+				alert("회원가입에 실패했습니다!");
 			});
 	};
 
@@ -107,40 +115,30 @@ const SignUp = () => {
 				<Label>아이디</Label>
 				<Input
 					type="text"
-					onChange={(e) => {
-						setUserIdInput(e.target.value);
-					}}
+					onChange={(e) => {setUserIdInput(e.target.value);}}
 				/>
+				<Button onClick={idCheck}>id 중복확인</Button>
 				<Label>비밀번호</Label>
 				<Input
 					type="password"
-					onChange={(e) => {
-						setPasswordInput(e.target.value);
-					}}
+					onChange={handlePasswordChange}
 				/>
 				<Label>비밀번호 재확인</Label>
 				<Input
 					type="password"
-					onChange={(e) => {
-						setPasswordCheck(e.target.value);
-					}}
+					onChange={handlePasswordCheckChange}
 				/>
 				<Label>이름</Label>
 				<Input
 					type="text"
-					onChange={(e) => {
-						setnameInput(e.target.value);
-					}}
+					onChange={(e) => {setnameInput(e.target.value);}}
 				/>
 				<Label>학번</Label>
 				<Input
 					type="text"
-					onChange={(e) => {
-						setstudent_numberInput(e.target.value);
-					}}
+					onChange={(e) => {setstudent_numberInput(e.target.value);}}
 				/>
 				<Button onClick={registeraxios}>가입완료</Button>
-				<Button onClick={idCheck}>가입</Button>
 			</SignUpForm>
 		</SignUpContent>
 	);
