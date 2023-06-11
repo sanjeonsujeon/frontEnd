880e14f1fa27b764d03576461dcea89c84423fb2
Hamtoto <gohg0901@naver.com>
2023-06-12 00:51:43 +0900

    api 경로 수정 및 코드 정리

--- src/pages/SginUp.jsx
+++ src/pages/SginUp.jsx
@@ -13,124 +13,89 @@ const SignUp = () => {
 	const [pwcheck, setPasswordCheck] = useState("");
 	const [name, setnameInput] = useState("");
 	const [st_number, setstudent_numberInput] = useState("");
-   
+
 	const navigate = useNavigate();
-	const url = ""
 
-	
-
-	const PwCheck = () => {// 확인 후 가입 진행 코딩 필요
-		console.log("lll");
+	const PwCheck = () => {
 		axios
-		  .post('localhost:8080/check',{
-			PW: pw,
-			PWCK: pwcheck,
-		  }).then((response) => {
-			console.log(response);
-			alert("회원가입 성공");
-			if ((response.status = 200)){
-				return navigate("/Login")
-			}
-		  }).catch((error) =>{
-			//console.log(error)
-            // ... 회원가입 실패 처리
-            // 노력했다 알아서 추가하렴~ 오전 9시 12분 안잔지 22시간째
-		  })
+			.post('localhost:8080/check', {
+				PW: pw,
+				PWCK: pwcheck,
+			}).then((response) => {
+				if ((response.status = 200)) {
+					return navigate("/Login")
+				}
+			}).catch((error) => {
+
+			})
 	}
 
-	const requestData = {user_id, pw, name, st_number}
+	const requestData = { user_id, pw, name, st_number }
 	const registeraxios = () => {
-		console.log("Registration Data:", requestData);
 		axios
-		  .post("http://localhost:8080/api/signup-pp", requestData)
-		  .then((response) => {
-			console.log(response);
-			alert("");
-			if ((response.status = 200)){
-				return alert("인증 완료");
-			}
-			console.log("lll");
-		  }).catch((error) =>{
-			//console.log(error)
-            // ... 회원가입 실패 처리
-            // 노력했다 알아서 추가하렴~ 오전 9시 12분 안잔지 22시간째
-		  })
+			.post("http://172.22.200.51:8080/api/signup-pp", requestData)
+			.then((response) => {
+				if ((response.status = 200)) {
+					return alert("회원가입 완료"), navigate("/Login");
+				}
+			}).catch((error) => {
+				return alert("회원가입에 실패했습니다!");
+			})
 	}
-
-    const play = () => {
-		alert("회원가입 성공")
-        navigate('/');
-    };
 
 	return (
-		<div className="signup">
-		  <div className="signup_input">
-		  <label>UserID</label>
-		  <br/>
-		  <input
-			type="text"
-			placeholder="username..."
-			onChange={(e) => {
-				setUserIdInput(e.target.value);
-			}}
-		  />
-		  </div>
-		  <div className="signup_input">
-		  <label>Password</label>
-		  <br/>
-		  <input
-			type="password"
-			placeholder="password..."
-			onChange={(e) => {
-				setPasswordInput(e.target.value);
-			}}
-			
-		  />
-		  </div>
-		  <div className="signup_input">
-		  <label>CheckPassword</label>
-		  <br/>
-		  <input
-			type="password"
-			placeholder="passwordCheck"
-			onChange={(e) => {
-				setPasswordCheck(e.target.value);
-			}}
-			
-		  />
-		  </div>
-		  <div className="signup_input">
-		  <label>UserName</label>
-		  <br/>
-		  <input
-			type="text"
-			placeholder="name..."
-			onChange={(e) => {
-			  setnameInput(e.target.value);
-			}}
-			
-		  />
-		   </div>
-		  <div className="signup_input">
-		  <label>Student_Number</label>
-		  <br/>
-		  <input
-			type="number"
-			placeholder="name..."
-			onChange={(e) => {
-			  setstudent_numberInput(e.target.value);
-			}}
-			
-		  />
-		  
-		  </div>
-		  <button onClick={registeraxios}>regist</button>
-		  <button onClick={play}>ppop</button>
-		  <button onClick={PwCheck}>CheckPW</button>
-				
-		</div>
-		
-	  );
+		<SignUpContent>
+			<div className="signup">
+				<div className="signup_input">
+					<label>UserID</label>
+					<br />
+					<input
+						type="text"
+						placeholder="username..."
+						onChange={(e) => { setUserIdInput(e.target.value); }}
+					/>
+				</div>
+				<div className="signup_input">
+					<label>Password</label>
+					<br />
+					<input
+						type="password"
+						placeholder="password..."
+						onChange={(e) => { setPasswordInput(e.target.value); }}
+					/>
+				</div>
+				<div className="signup_input">
+					<label>CheckPassword</label>
+					<br />
+					<input
+						type="password"
+						placeholder="passwordCheck"
+						onChange={(e) => { setPasswordCheck(e.target.value); }}
+					/>
+				</div>
+				<div className="signup_input">
+					<label>UserName</label>
+					<br />
+					<input
+						type="text"
+						placeholder="name..."
+						onChange={(e) => { setnameInput(e.target.value); }}
+					/>
+				</div>
+				<div className="signup_input">
+					<label>Student_Number</label>
+					<br />
+					<input
+						type="number"
+						placeholder="name..."
+						onChange={(e) => { setstudent_numberInput(e.target.value); }}
+					/>
+				</div>
+				<button onClick={registeraxios}>regist</button>
+				<button onClick={PwCheck}>CheckPW</button>
+			</div>
+		</SignUpContent>
+	);
 };
 
 export default SignUp;
