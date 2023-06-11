3e2d2bbb531876433e7df36e3058c552fb43a1b7
AoiTuNa <bestspark123@naver.com>
2023-06-12 04:48:04 +0900

    불필요 코드 제거

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 const HeaderHeight = "80px";
 const FooterHeight = "80px";
 const BodyWidth = "1100px";
-<<<<<<< HEAD
-const serverAddress = "localhost:8080";
-=======
->>>>>>> 94e4e86f40d2550ddc5e030197b175390a0170c8
 
 const value = {
     HeaderHeight,
