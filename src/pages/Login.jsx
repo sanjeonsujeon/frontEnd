b986597421b49697ae7238f5035db6b096591ce8
AoiTuNa <bestspark123@naver.com>
2023-06-11 21:54:13 +0900

    ;;;

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import Button from "../Component/Button";
@@ -26,16 +27,60 @@ const OButton = styled(Button)`
 
 `
 
-const Login = (props) => {
+const Login = () => {
 
     const navigate = useNavigate();
 
+    const [UserID, setUserID] = useState("");
+    const [Password, setPassword] = useState("");
+
+
+    const onSubmit = (event) => {
+      console.log("lll");
+        event.preventDefault();
+        axios
+          .post("/api/login", {
+            UserID: UserID,
+            Password: Password,
+          })
+          .then((res) => {
+            console.log(res);
+            localStorage.setItem("authorization",res.headers.authorization);
+            localStorage.setItem("refreshtoken", res.headers.refershtoken);
+             setInterval(onSilentRefresh, 60000);
+             onSilentRefresh();
+          })
+          .catch((Error) => {
+            console.log(Error)
+            alert("일치하는 회원 정보가 없습니다.");
+          })
+    }
+
+    const onSilentRefresh = () => {
+        axios.post("/api/oauth/token", {
+            authorization: localStorage.getItem("authorization"),
+            refershtoken: localStorage.getItem("refreshtoken")
+        })
+            .then((response) => {
+                console.log(response);
+                console.log("연장됨")
+                localStorage.setItem("authorization",response.headers.authorization);
+                localStorage.setItem("refreshtoken",response.headers.refreshtoken);
+          setInterval(onSilentRefresh, 60000);
+        })
+        .catch(error => {
+          alert("연장실패.");
+          console.log(error)
+            // ... 로그인 실패 처리
+            // 노력했다 알아서 추가하렴~ 오전 9시 2분 안잔지 22시간째
+            });
+    }
     const login = () => {
         navigate('/');
     };
 
     const signup = () => {
-        navigate('/sginup');
+        navigate('/signup');
     };
 
 	return (
@@ -43,11 +88,19 @@ const Login = (props) => {
 			<LoginContent>
                 <h2>Login</h2>
                 <InputForm>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                 <input
+                   type="text"
+                   placeholder="ID"
+                   onChange={(e) => setUserID(e.target.value)}
+                  />
+                   <input
+                   type="password"
+                   placeholder="Password"
+                   onChange={(e) => setPassword(e.target.value)}
+                  />
                 </InputForm>
                 <ButtonForm>
-                    <OButton onClick={login}>Login</OButton>
+                    <OButton onClick={onSubmit}>Login</OButton>
                     <OButton onClick={signup}>Sign Up</OButton>
                 </ButtonForm>
 			</LoginContent>
